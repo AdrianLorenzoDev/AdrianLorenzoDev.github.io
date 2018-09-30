dbcf38e0b9860ecde26b60bd8ab426409aad56bc
Adrián Lorenzo Melián <defuzah@gmail.com>
2018-09-30 12:56:29 +0100

    Added section to gear page. Corrected indentation. Added empty layout for future test. Restructured css classes

--- res/addons.js
+++ res/addons.js
@@ -1,5 +1,5 @@
 //Fade in when ready and tooltip
 $(document).ready(function(){
-  $('#sum').fadeIn(2500);
+  $('#content').fadeIn(2500);
   $('[data-toggle="tooltip"]').tooltip({container:'body', trigger:'hover', placement:'bottom'});
 });
