3cf07e03852bc43e6192c26c0784778e1e73b56d
Adrián Lorenzo Melián <adrianlorenzo@Adrians-MacBook-Air.local>
2018-11-04 22:11:25 +0000

    Fixed fade in. Integrated resume. Fixed layout issues

--- res/js/addons.js
+++ res/js/addons.js
@@ -1,5 +1,5 @@
 //Fade in when ready and tooltip
 $(document).ready(function(){
-  $('#content').fadeIn(2500);
+  $('#content').fadeIn(2200);
   $('[data-toggle="tooltip"]').tooltip({container:'body', trigger:'hover', placement:'bottom'});
 });
