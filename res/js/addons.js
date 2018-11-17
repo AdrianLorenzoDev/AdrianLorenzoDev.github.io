383cd6a8694c3d5c86dccdae754265d047272e26
AdrianLorenzoDev <defuzah@gmail.com>
2018-11-17 16:18:08 +0000

    Added metadata, improved accesibility

--- res/js/addons.js
+++ res/js/addons.js
@@ -1,5 +1,5 @@
 //Fade in when ready and tooltip
 $(document).ready(function(){
-  $('#content').fadeIn(2200);
+  $('#content').fadeIn(600);
   $('[data-toggle="tooltip"]').tooltip({container:'body', trigger:'hover', placement:'bottom'});
 });
