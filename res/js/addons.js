3e7ab84ff1de77bbf9defee7899e68d8e8235163
AdrianLorenzoDev <defuzah@gmail.com>
2019-01-08 22:24:06 +0000

    Removed gear section. Cleaned webpage

--- res/js/addons.js
+++ res/js/addons.js
@@ -1,5 +1,5 @@
 //Fade in when ready and tooltip
 $(document).ready(function(){
-  $('#content').fadeIn(600);
+  $('#content').fadeIn(900);
   $('[data-toggle="tooltip"]').tooltip({container:'body', trigger:'hover', placement:'bottom'});
 });
