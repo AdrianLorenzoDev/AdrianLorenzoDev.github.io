cc95aed67021228dfa49a294318cd1f47572f071
AdrianLorenzoDev <defuzah@gmail.com>
2019-01-24 22:11:01 +0000

    Implicated footer into fade in effect. Fixed indentation of the code

--- res/js/addons.js
+++ res/js/addons.js
@@ -1,5 +1,5 @@
 //Fade in when ready and tooltip
-$(document).ready(function(){
+$(document).ready(function () {
   $('#content').fadeIn(900);
-  $('[data-toggle="tooltip"]').tooltip({container:'body', trigger:'hover', placement:'bottom'});
+  $('#footer').fadeIn(900);
 });
